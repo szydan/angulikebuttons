1e9ee2b05548da8a8997385a9abacc8185ff0d23
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2016-02-02 01:34:40 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+angulikebuttons
+========
+
+This module is based on https://github.com/cornflourblue/angulike
+
+
+AngularJS directives for social sharing buttons - Facebook Like, Google+, Twitter and Pinterest 
+
+###Demo
+
+To see a demo and further details go to http://jasonwatmore.com/post/2014/08/01/AngularJS-directives-for-social-sharing-buttons-Facebook-Like-GooglePlus-Twitter-and-Pinterest.aspx
+
+###Installation
+
+Install using bower: `bower install angulikebuttons`
+
+Alternatively download the code and include the angulikebuttons.js file in your page.
+
+Add the 'angulike' directive as a dependency of your AngularJS application:
+
+```javascript
+angular.module('myApp', ['angulike']);
+```
+
+###Usage
+
+####Facebook Like
+
+Create an empty div with the *fb-like* attribute.
+
+```html
+<div fb-like></div>
+```
+
+To use a custom URL for the facebook like button:
+
+```html
+<div fb-like="myModel.Url"></div>
+```
+
+####Google+
+
+Create an empty div with the *google-plus* attribute.
+
+```html
+<div google-plus></div>
+```
+
+To use a custom URL for the google plus button:
+
+```html
+<div google-plus="myModel.Url"></div>
+```
+
+####Twitter
+
+Create an empty div with the *tweet* attribute, the value of the attribute contains the name of the model object for the tweet text.
+
+```html
+<div tweet="myModel.Name"></div>
+```
+
+To use a custom URL for the tweet button:
+
+```html
+<div tweet="myModel.Name" tweet-url="myModel.Url"></div>
+```
+
+####Pinterest
+
+Create an empty div with the *pin-it* and *pin-it-image* attributes, the pin-it attribute contains the name of the model object for the 
+description and the pin-it-image attribute contains the name of the model object for the image url:
+
+```html
+<div pin-it="myModel.Name" pin-it-image="myModel.ImageUrl"></div>
+```
+
+To use a custom URL for the pinterest button:
+
+```html
+<div pin-it="myModel.Name" pin-it-image="myModel.ImageUrl" pin-it-url="myModel.Url"></div>
+```
---
+++ angulikebuttons.js
@@ -0,0 +1,229 @@
+﻿/**
+ * AngularJS directives for social sharing buttons - Facebook Like, Google+, Twitter and Pinterest 
+ * based on Jason Watmore <jason@pointblankdevelopment.com.au> (http://jasonwatmore.com)
+ * @version 1.0.0
+ */
+(function () {
+    
+  var loadScript = function (id, url, callback) {
+    var s = window.document.getElementById(id);
+    if(!s) {
+        s = window.document.createElement('script'); // use global document since Angular's $document is weak
+        s.type = 'text/javascript';
+        s.src = url;
+        s.async = true;
+        s.id = id;
+        s.onload = function() {
+          if(callback) {
+          callback();
+        }
+        };
+        window.document.body.appendChild(s);
+    } else {
+      if(callback) {
+        callback();
+      }
+    }
+  };
+
+
+
+
+    angular.module('angulikebuttons', [])
+
+      .directive('fbLike', [
+          '$window', '$rootScope', function ($window, $rootScope) {
+              return {
+                  restrict: 'A',
+                  scope: {
+                      fbLike: '=?'
+                  },
+                  link: function (scope, element, attrs) {
+                      if (!$window.FB) {
+                          // Load Facebook SDK if not already loaded
+                          var lang = $rootScope.facebookLang;
+                          if(!lang) {
+                            lang = 'en_US';
+                          }
+                          loadScript(
+                            'fb-script', '//connect.facebook.net/'+lang+'/sdk.js', 
+                            function(){
+                              $window.FB.init({
+                                appId: $rootScope.facebookAppId,
+                                xfbml: true,
+                                version: 'v2.0'
+                              });
+                              renderLikeButton();
+                            }
+                          );    
+
+                      } else {
+                          renderLikeButton();
+                      }
+
+                      var watchAdded = false;
+                      function renderLikeButton() {
+                          if (!!attrs.fbLike && !scope.fbLike && !watchAdded) {
+                              // wait for data if it hasn't loaded yet
+                              watchAdded = true;
+                              var unbindWatch = scope.$watch('fbLike', function (newValue, oldValue) {
+                                  if (newValue) {
+                                      renderLikeButton();
+                                      
+                                      // only need to run once
+                                      unbindWatch();
+                                  }
+                                  
+                              });
+                              return;
+                          } else {
+                              element.html('<div class="fb-like"' + (!!scope.fbLike ? ' data-href="' + scope.fbLike + '"' : '') + ' data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
+                              $window.FB.XFBML.parse(element.parent()[0]);
+                          }
+                      }
+                  }
+              };
+          }
+      ])
+
+      .directive('googlePlus', [
+          '$window', function ($window) {
+              return {
+                  restrict: 'A',
+                  scope: {
+                      googlePlus: '=?'
+                  },
+                  link: function (scope, element, attrs) {
+                      if (!$window.gapi) {
+                          // Load Google SDK if not already loaded
+                          loadScript('google-sdk', '//apis.google.com/js/platform.js', function () {
+                              renderPlusButton();
+                          });
+                      } else {
+                          renderPlusButton();
+                      }
+
+                      var watchAdded = false;
+                      function renderPlusButton() {
+                          if (!!attrs.googlePlus && !scope.googlePlus && !watchAdded) {
+                              // wait for data if it hasn't loaded yet
+                              watchAdded = true;
+                              var unbindWatch = scope.$watch('googlePlus', function (newValue, oldValue) {
+                                  if (newValue) {
+                                      renderPlusButton();
+
+                                      // only need to run once
+                                      unbindWatch();
+                                  }
+
+                              });
+                              return;
+                          } else {
+                              element.html('<div class="g-plusone"' + (!!scope.googlePlus ? ' data-href="' + scope.googlePlus + '"' : '') + ' data-size="medium"></div>');
+                              $window.gapi.plusone.go(element.parent()[0]);
+                          }
+                      }
+                  }
+              };
+          }
+      ])
+      .directive('tweet', [
+          '$window', '$location',
+          function ($window, $location) {
+              return {
+                  restrict: 'A',
+                  scope: {
+                      tweet: '=',
+                      tweetUrl: '='
+                  },
+                  link: function (scope, element, attrs) {
+                      if (!$window.twttr) {
+                          // Load Twitter SDK if not already loaded
+                          loadScript('twitter-sdk', '//platform.twitter.com/widgets.js', function () {
+                              renderTweetButton();
+                          });
+                      } else {
+                          renderTweetButton();
+                      }
+
+                      var watchAdded = false;
+                      function renderTweetButton() {
+                          if (!scope.tweet && !watchAdded) {
+                              // wait for data if it hasn't loaded yet
+                              watchAdded = true;
+                              var unbindWatch = scope.$watch('tweet', function (newValue, oldValue) {
+                                  if (newValue) {
+                                      renderTweetButton();
+                                  
+                                      // only need to run once
+                                      unbindWatch();
+                                  }
+                              });
+                              return;
+                          } else {
+                              element.html('<a href="https://twitter.com/share" class="twitter-share-button" data-text="' + scope.tweet + '" data-url="' + (scope.tweetUrl || $location.absUrl()) + '">Tweet</a>');
+                              $window.twttr.widgets.load(element.parent()[0]);
+                          }
+                      }
+                  }
+              };
+          }
+      ])
+
+      .directive('pinIt', [
+          '$window', '$location',
+          function ($window, $location) {
+              return {
+                  restrict: 'A',
+                  scope: {
+                      pinIt: '=',
+                      pinItImage: '=',
+                      pinItUrl: '='
+                  },
+                  link: function (scope, element, attrs) {
+                      if (!$window.parsePins) {
+                          // Load Pinterest SDK if not already loaded
+                          (function (d) {
+                              var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+                              p.type = 'text/javascript';
+                              p.async = true;
+                              p.src = '//assets.pinterest.com/js/pinit.js';
+                              p['data-pin-build'] = 'parsePins';
+                              p.onload = function () {
+                                  if (!!$window.parsePins) {
+                                      renderPinItButton();
+                                  } else {
+                                      setTimeout(p.onload, 100);
+                                  }
+                              };
+                              f.parentNode.insertBefore(p, f);
+                          }($window.document));
+                      } else {
+                          renderPinItButton();
+                      }
+
+                      var watchAdded = false;
+                      function renderPinItButton() {
+                          if (!scope.pinIt && !watchAdded) {
+                              // wait for data if it hasn't loaded yet
+                              watchAdded = true;
+                              var unbindWatch = scope.$watch('pinIt', function (newValue, oldValue) {
+                                  if (newValue) {
+                                      renderPinItButton();
+                                      
+                                      // only need to run once
+                                      unbindWatch();
+                                  }
+                              });
+                              return;
+                          } else {
+                              element.html('<a href="//www.pinterest.com/pin/create/button/?url=' + (scope.pinItUrl || $location.absUrl()) + '&media=' + scope.pinItImage + '&description=' + scope.pinIt + '" data-pin-do="buttonPin" data-pin-config="beside"></a>');
+                              $window.parsePins(element.parent()[0]);
+                          }
+                      }
+                  }
+              };
+          }
+      ]);
+
+})();
